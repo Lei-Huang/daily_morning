94f3a0a30305817b3458bd507350438ddfcc4cf6
LeiH <lei.huang.au@gmail.com>
2022-08-14 22:33:24 +1000

    updated words to jin ri yun shi

--- test.py
+++ test.py
@@ -4,10 +4,14 @@ def get_weather():
     url = "http://autodev.openspeech.cn/csp/api/v2.1/weather?openId=aiuicus&clientType=android&sign=android&city=堪培拉"
     res = requests.get(url).json()
     weather = res['data']['list'][0]
-    print(weather)
     return (weather['weather'], math.floor(weather['temp']), weather['humidity'], math.floor(weather['low']), math.floor(weather['high']), weather['wind'])
 
 wea, temperature, humidity, low, high, wind = get_weather()
-data = {"weather":{"value":wea},"temperature":{"value":temperature},"humidity":{"value":humidity},"low_temp":{"value":low},"high_temp":{"high":high},"wind":{"wind":wind}}
+data = {"weather":{"value":wea},"temperature":{"value":temperature},"humidity":{"value":humidity},"low_temp":{"value":low},"high_temp":{"value":high},"wind":{"value":wind}}
 
-print(data)
+
+def get_words():
+    words = requests.get("http://api.tianapi.com/star/index?key=419186fbca431df736e8a80f9bdcadca&astro=virgo").json()
+    return words['newslist'][-1]['content']
+
+print(get_words())
